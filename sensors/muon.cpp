fbdb40d2758f8c01bb243a5d663e0d483cf25c89
Kazuma8 <kazumasa3miura@gmail.com>
2024-11-13 08:15:36 +0000

    Fix typo

--- sensors/muon.cpp
+++ sensors/muon.cpp
@@ -12,6 +12,7 @@
 const long double cal[] = {-9.085681659276021e-27, 4.6790804314609205e-23, -1.0317125207013292e-19,
   1.2741066484319192e-16, -9.684460759517656e-14, 4.6937937442284284e-11, -1.4553498837275352e-08,
    2.8216624998078298e-06, -0.000323032620672037, 0.019538631135788468, -0.3774384056850066, 12.324891083404246};
+
 const int cal_max = 1023;
 */
 
@@ -19,6 +20,7 @@ void initMuon() {
      //Use UART
     uart_init(UART_ID, BAUD_RATE);
     //adc_gpio_init(U_PIN);
+    
     //set TX RX pins
     gpio_set_function(UART_TX_PIN, UART_FUNCSEL_NUM(UART_ID, UART_TX_PIN));
     gpio_set_function(UART_RX_PIN, UART_FUNCSEL_NUM(UART_ID, UART_RX_PIN));
@@ -30,6 +32,7 @@ size_t uart_read_non_blocking(uint8_t *buffer, size_t length) {
         buffer[bytes_read++] = uart_getc(UART_ID);
     }
     return bytes_read;
+}
 
 void readMuon(struct STATE *state) {
     //adc_select_input(2);
